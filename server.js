e1bfc029abf9daf84031d3b408b179f9acb01148
Dom Vu <Dominicvu36@gmail.com>
2020-05-02 16:04:56 -0600

    update again

--- server.js
+++ server.js
@@ -74,7 +74,21 @@ router.route('/thermostat')
     });
 
 router.post('/button', function(req, res) {
-    res.send({status: 200, message: "All rooms will be equalized to 70"});
+    const json = {
+        highlevel: {
+            state: "false"
+        },
+        monitors: {
+            master_bedroom: 70,
+            guest_bedroom: 70,
+            kitchen: 70,
+            living_room: 70,
+            basement: 70
+        },
+        actions: {
+        }
+    };
+    res.json(json);
 }).post('/toggle', function (req, res) {
     var query = Object.keys(req.query).length === 0 ? null : req.query;
 
